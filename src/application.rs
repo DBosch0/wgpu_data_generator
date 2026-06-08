4861afd23d3787548d4363af77c84b5b60b17943
DavidBosch-Code <davidbos@chalmers.se>
2026-06-08 16:19:01 +0200

    application and empty window

--- src/application.rs
+++ src/application.rs
@@ -2,51 +2,133 @@
 
 use std::sync::Arc;
 
-use winit::{application::ApplicationHandler, window::Window};
+use winit::{
+    application::ApplicationHandler,
+    event::{KeyEvent, WindowEvent},
+    event_loop::ActiveEventLoop,
+    keyboard::{Key, NamedKey},
+    window::{self, Window},
+};
 
 use crate::{GpuContext, SurfaceWrapper};
+
+struct ApplicationState;
+
+impl ApplicationState {
+    fn new() -> Self {
+        // TODO State of the application
+        Self
+    }
+
+    fn resize(&mut self) {}
+
+    fn render(&mut self, view: &wgpu::TextureView, device: &wgpu::Device, queue: &wgpu::Queue) {}
+
+    fn update(&mut self, event: WindowEvent) {}
+}
 
 pub(crate) struct Application<'a> {
     title: &'static str,
     window: Option<Arc<Window>>,
-    surface: Option<SurfaceWrapper<'a>>,
+    surface: SurfaceWrapper<'a>,
     context: GpuContext,
+    state: Option<ApplicationState>,
 }
 
-impl Application<'_> {
-    pub(crate) fn new(title: &'static str, context: GpuContext) -> Self {
+impl<'a> Application<'a> {
+    pub(crate) fn new(
+        title: &'static str,
+        context: GpuContext,
+        surface: SurfaceWrapper<'a>,
+    ) -> Self {
         Self {
             title,
             window: None,
-            surface: None,
+            surface,
             context,
+            state: None,
         }
+    }
+
+    pub(crate) fn initialize(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = window::WindowAttributes::default().with_title(self.title);
+        self.window = Some(Arc::new(
+            event_loop
+                .create_window(window_attributes)
+                .expect("creating window"),
+        ));
+
+        self.surface.resume(
+            &self.context,
+            Arc::clone(self.window.as_ref().unwrap()),
+            false,
+        );
+        self.state = Some(ApplicationState::new());
     }
 }
 
 impl<'a> ApplicationHandler for Application<'a> {
-    fn new_events(
-        &mut self,
-        _event_loop: &winit::event_loop::ActiveEventLoop,
-        _cause: winit::event::StartCause,
-    ) {
-        todo!()
-    }
-
-    fn resumed(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {
-        todo!()
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        //after NewEvent::StartCause::Init this function is called. here we intialize
+        //the window, surface, and application state
+        self.initialize(event_loop);
     }
 
     fn window_event(
         &mut self,
-        _event_loop: &winit::event_loop::ActiveEventLoop,
+        event_loop: &winit::event_loop::ActiveEventLoop,
         _window_id: winit::window::WindowId,
-        _event: winit::event::WindowEvent,
+        event: winit::event::WindowEvent,
     ) {
-        todo!()
-    }
+        match event {
+            WindowEvent::Resized(size) => {
+                self.surface.resize(&self.context, size);
+                self.state.as_mut().unwrap().resize();
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        logical_key: Key::Named(NamedKey::Escape),
+                        ..
+                    },
+                ..
+            }
+            | WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        logical_key: Key::Character(s),
+                        ..
+                    },
+                ..
+            } if s == "r" => {
+                println!("{:#?}", self.context.instance.generate_report());
+            }
+            WindowEvent::RedrawRequested => {
+                if self.state.is_none() {
+                    return;
+                }
 
-    fn suspended(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {
-        todo!()
+                let frame = self.surface.acquire(&self.context);
+                let view = frame.texture.create_view(&wgpu::TextureViewDescriptor {
+                    format: Some(self.surface.config().view_formats[0]),
+                    ..wgpu::wgt::TextureViewDescriptor::default()
+                });
+
+                self.state.as_mut().unwrap().render(
+                    &view,
+                    &self.context.device,
+                    &self.context.queue,
+                );
+
+                self.window.as_ref().unwrap().pre_present_notify();
+                frame.present();
+
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => self.state.as_mut().unwrap().update(event),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
-use winit::event_loop::EventLoop;
+use std::sync::Arc;
+
+use winit::{dpi::PhysicalSize, event_loop::EventLoop, window::Window};
 
 use crate::application::Application;
 
@@ -59,8 +61,91 @@ async fn get_adapter_with_capabilities_or_from_env(
 }
 
 pub(crate) struct SurfaceWrapper<'a> {
-    pub(crate) surface: wgpu::Surface<'a>,
-    pub(crate) config: wgpu::SurfaceConfiguration,
+    pub(crate) surface: Option<wgpu::Surface<'a>>,
+    pub(crate) config: Option<wgpu::SurfaceConfiguration>,
+}
+
+impl<'a> SurfaceWrapper<'a> {
+    pub(crate) fn new() -> Self {
+        Self {
+            surface: None,
+            config: None,
+        }
+    }
+
+    pub(crate) fn config(&self) -> &wgpu::SurfaceConfiguration {
+        self.config.as_ref().unwrap()
+    }
+
+    pub(crate) fn resume(&mut self, context: &GpuContext, window: Arc<Window>, srgb: bool) {
+        // Window size is only actually valid after we enter the event loop.
+        let window_size = window.inner_size();
+        let width = window_size.width.max(1);
+        let height = window_size.height.max(1);
+
+        log::info!("Surface resume {window_size:?}");
+        self.surface = Some(
+            context
+                .instance
+                .create_surface(window)
+                .expect("Creating surface"),
+        );
+
+        let surface = self.surface.as_ref().unwrap();
+
+        let mut config = surface
+            .get_default_config(&context.adapter, width, height)
+            .expect("Surface not supported by platform");
+        if srgb {
+            // Not all platforms (WebGPU) support sRGB swapchains, so we need to use view formats
+            let view_format = config.format.add_srgb_suffix();
+            config.view_formats.push(view_format);
+        } else {
+            // All platforms support non-sRGB swapchains, so we can just use the format directly.
+            let format = config.format.remove_srgb_suffix();
+            config.format = format;
+            config.view_formats.push(format);
+        }
+        surface.configure(&context.device, &config);
+        self.config = Some(config);
+    }
+
+    pub(crate) fn acquire(&mut self, context: &GpuContext) -> wgpu::SurfaceTexture {
+        let surface = self.surface.as_ref().unwrap();
+
+        match surface.get_current_texture() {
+            wgpu::CurrentSurfaceTexture::Success(frame)
+            | wgpu::CurrentSurfaceTexture::Suboptimal(frame) => frame,
+            // Try again in this case
+            wgpu::CurrentSurfaceTexture::Timeout => match surface.get_current_texture() {
+                wgpu::CurrentSurfaceTexture::Success(frame)
+                | wgpu::CurrentSurfaceTexture::Suboptimal(frame) => frame,
+                _ => panic!("Failed to acquire next surface texture"),
+            },
+            // Reconfigure and then try again.
+            wgpu::CurrentSurfaceTexture::Occluded
+            | wgpu::CurrentSurfaceTexture::Outdated
+            | wgpu::CurrentSurfaceTexture::Lost
+            | wgpu::CurrentSurfaceTexture::Validation => {
+                surface.configure(&context.device, self.config());
+                match surface.get_current_texture() {
+                    wgpu::CurrentSurfaceTexture::Success(frame)
+                    | wgpu::CurrentSurfaceTexture::Suboptimal(frame) => frame,
+                    _ => panic!("Failed to acquire next surface texture"),
+                }
+            }
+        }
+    }
+
+    pub(crate) fn resize(&mut self, context: &GpuContext, size: PhysicalSize<u32>) {
+        log::info!("Surface resize {size:?}");
+
+        let config = self.config.as_mut().unwrap();
+        config.width = size.width.max(1);
+        config.height = size.height.max(1);
+        let surface = self.surface.as_ref().unwrap();
+        surface.configure(&context.device, config);
+    }
 }
 
 #[derive(Debug)]
@@ -127,10 +212,11 @@ async fn start(title: &'static str) {
 
     let backends = wgpu::Instance::enabled_backend_features();
     log::info!("Enabled Backends {:?}", backends);
+    let surface = SurfaceWrapper::new();
     let context = GpuContext::init_async().await;
     log::info!("Create GpuContext");
 
-    let mut application = Application::new(title, context);
+    let mut application = Application::new(title, context, surface);
     let event_loop = EventLoop::new().expect("Can construct an event loop");
 
     log::info!("Entering Event Loop");
@@ -140,6 +226,5 @@ async fn start(title: &'static str) {
 }
 
 fn main() {
-    log::debug!("test");
     pollster::block_on(start("obj_viewer"))
 }
