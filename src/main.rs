3067b0e24c667f81a4676ed4e159078e9f0f36c8
DavidBosch-Code <davidbos@chalmers.se>
2026-06-08 13:56:06 +0200

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn init_logger() {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .filter_module("wgpu_core", log::LevelFilter::Info)
+        .filter_module("wgpu_hall", log::LevelFilter::Error)
+        .filter_module("naga", log::LevelFilter::Error)
+        .parse_default_env()
+        .build();
+}
+
 async fn start(title: &'static str) {
     todo!()
 }
